87119215d9c233f824dd31a06a42af6a81e2f9e0
Kacper <kacper@eduroam-249-156.nomadic.bris.ac.uk>
2012-02-28 15:10:51 +0000

    hard tabs -> soft tabs

--- src/plumber/Game.java
+++ src/plumber/Game.java
@@ -3,13 +3,12 @@ package plumber;
 public class Game 
 {
 
-	/**
-	 * @param args Arguments to the program - can be ignored
-	 */
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-
+    /**
+     * @param args Arguments to the program - can be ignored
+     */
+    public static void main(String[] args) 
+    {
+        // TODO Auto-generated method stub
+        //test
 	}
-
 }
