8ab8f01a6fa9bf43b2164e29fb0cd6d25a3d0f73
Maciej Kumorek <mkumorek@gmail.com>
2012-02-28 14:57:04 +0000

    Initial commit

---
+++ src/plumber/Game.java
@@ -0,0 +1,15 @@
+package plumber;
+
+public class Game 
+{
+
+	/**
+	 * @param args Arguments to the program - can be ignored
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
