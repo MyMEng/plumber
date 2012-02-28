9fb54090e5f6bca5678109dcc923c328a7bae1ce
Kacper <kacper@eduroam-249-156.nomadic.bris.ac.uk>
2012-02-28 15:39:27 +0000

    merged kacper_test

--- src/plumber/Game.java
+++ src/plumber/Game.java
@@ -10,8 +10,14 @@ public class Game
      */
     public static void main(String[] args) 
     {
+<<<<<<< HEAD
         JPanel panel = new JPanel(new BorderLayout());
         panel.setPreferredSize(new Dimension(800, 600));
         panel.setVisible(true);
+=======
+        // TODO Auto-generated method stub
+        //test
+        //druga zmiana
+>>>>>>> kacper_test
     }
 }
